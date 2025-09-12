9cb2161077918cbfe849d76fce1496472b2d3183
mitsuarizqi <dayoosasha@gmail.com>
2025-09-13 06:43:05 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,64 +1,39 @@
 {
-  description = "NixOS + Hyprland + Yuki + Omarchy setup";
+  description = "NixOS config with Hyprland, Yuki, Omarchy";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; #
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     home-manager.url = "github:nix-community/home-manager/release-25.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     yuki.url = "github:raexera/yuki";
+    omarchy.url = "github:ChrisTitusTech/omarchy-titus";
   };
 
-  outputs = { self, nixpkgs, home-manager, yuki, ... }@inputs:
+  outputs = { self, nixpkgs, home-manager, yuki, omarchy, ... }@inputs:
     let
       system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in {
       nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
         inherit system;
         modules = [
-          ./hardware-configuration.nix
-          {
-            # Bootloader
-            boot.loader.systemd-boot.enable = true;
-            boot.loader.efi.canTouchEfiVariables = true;
+          ./configuration.nix
 
-            # Network
-            networking.networkmanager.enable = true;
-
-            # Display Manager + Hyprland
-            services.xserver.enable = true;
-            services.xserver.displayManager.sddm.enable = true;
-            programs.hyprland.enable = true;
-
-            # User
-            users.users.rizqi = {
-              isNormalUser = true;
-              extraGroups = [ "wheel" "networkmanager" ];
-              shell = pkgs.zsh; # atau bash
-            };
-
-            # Packages
-            environment.systemPackages = with pkgs; [
-              git wget curl vim micro
-              hyprland waybar dunst rofi-wayland alacritty
-            ];
-
-            # Time + locale
-            time.timeZone = "Asia/Jakarta";
-            i18n.defaultLocale = "en_US.UTF-8";
-
-            # Sudo
-            security.sudo.enable = true;
-
-            # Enable home-manager system wide
-            programs.home-manager.enable = true;
-          }
-
-          # Import Yuki home-manager config
           home-manager.nixosModules.home-manager
           {
-            home-manager.users.yourname = {
-              imports = [ yuki.nixosModules.default ];
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.rizqi = {
+              imports = [
+                yuki.homeManagerModules.default
+                omarchy.homeManagerModules.default
+              ];
             };
           }
         ];
