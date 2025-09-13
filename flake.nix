6f5ca4aaf1394e89cd4aaee2c868386b5d135ce3
mitsuarizqi <dayoosasha@gmail.com>
2025-09-13 11:41:52 +0700

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "NixOS basic with Yuki later";
+  description = "NixOS basic with Yuki home";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
@@ -24,6 +24,7 @@
             useGlobalPkgs = true;
             useUserPackages = true;
             users.rizqi = {
+              imports = [ ./yuki/home ];  # Kalau path Yuki ada; hapus kalau error
               home.username = "rizqi";
               home.homeDirectory = "/home/rizqi";
               home.stateVersion = "25.05";
@@ -33,6 +34,27 @@
           time.timeZone = "Asia/Jakarta";
           networking.networkmanager.enable = true;
           system.stateVersion = "25.05";
+
+          # Bootloader GRUB untuk UEFI + dual-boot Windows
+          boot.loader = {
+            efi = {
+              canTouchEfiVariables = true;
+              efiSysMountPoint = "/boot";  # Mount ESP di /boot
+            };
+            grub = {
+              enable = true;
+              device = "nodev";  # UEFI: Jangan ganti
+              efiSupport = true;
+              useOSProber = true;  # Auto-detect Windows
+              configurationLimit = 5;
+            };
+          };
+
+          # Mount ESP (ganti /dev/sda1 dengan ESP FAT32-mu dari lsblk)
+          fileSystems."/boot" = {
+            device = "/dev/sda2";
+            fsType = "vfat";
+          };
         }
       ];
     };
