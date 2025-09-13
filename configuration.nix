13cede298b6b5c51834d3706e13e8f54a21f26e7
mitsuarizqi <dayoosasha@gmail.com>
2025-09-13 12:51:43 +0700

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -3,53 +3,36 @@
 {
   imports = [
     ./hardware-configuration.nix
+    # Modules dari yuki: ./modules/*.nix (Hyprland, ZSH, Kitty, VS Code, dll.)
   ];
 
-  # Bootloader: GRUB on SATA (/dev/sda)
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.efi.canTouchEfiVariables = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # GRUB buat dual-boot EFI
   boot.loader.grub = {
     enable = true;
-    devices = [ "/dev/sda2" ]; # install GRUB ke SATA disk
-    useOSProber = true;       # deteksi OS lain
+    device = "nodev";
+    efiSupport = true;
+    useOSProber = true;  # Auto-detect Windows di NVMe
+    enableCryptodisk = true;  # Kalau Windows encrypted
   };
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.efi.efiSysMountPoint = "/boot";
 
-  networking.hostName = "nixos-btw"; # bisa diganti sesuai mau
-
+  networking.hostName = "yuki";
   time.timeZone = "Asia/Jakarta";
-  i18n.defaultLocale = "en_US.UTF-8";
 
-  console.keyMap = "us";
-  networking.networkmanager.enable = true;
-
-  users.users.rizqi = {
+  users.users.raexera = {
     isNormalUser = true;
     extraGroups = [ "wheel" "networkmanager" ];
-    packages = with pkgs; [ vim git firefox ];
+    # Tambah SSH keys kalau ada di yuki
   };
 
-  environment.systemPackages = with pkgs; [
-    wget curl htop neovim micro
-  ];
+  environment.systemPackages = with pkgs; [ vim git ];
+  # Dari yuki: programs.hyprland.enable = true; programs.zsh.enable = true; programs.vscode.enable = true; dll.
 
-  # Opsional: GUI KDE
-  services.xserver = {
-    enable = true;
-    displayManager.sddm.enable = true;
-    desktopManager.plasma5.enable = true;
-    xkb.layout = "us";
-  };
+  services.xserver.enable = true;
+  # Hyprland dari yuki: programs.hyprland.enable = true; services.desktopManager.plasma6.enable = false;
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-  services.openssh.enable = true;
-  networking.firewall.enable = true;
+  system.stateVersion = "24.05";  # Sesuaikan versi NixOS
 }
